a30ef43d7cee8b9538eb144203299998775c3bfa
Carlos Castillo <cookieo9@silver-bullet.home-lan>
2012-08-08 18:02:56 -0700

    Added experimental HTTP sub-package.

---
+++ v2/resources/http/http.go
@@ -0,0 +1,36 @@
+package http
+
+import (
+	"github.com/cookieo9/resources-go/v2/resources"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+type HttpBundle struct {
+	BaseURL *url.URL
+}
+
+func NewBundle(baseurl string) (resources.Bundle, error) {
+	u, err := url.Parse(baseurl)
+	if err != nil {
+		return nil, err
+	}
+	return &HttpBundle{BaseURL: u}, nil
+}
+
+func (hb *HttpBundle) Open(path string) (io.ReadCloser, error) {
+	dest, err := hb.BaseURL.Parse(path)
+	if err != nil {
+		return nil, err
+	}
+	response, err := http.Get(dest.String())
+	if err != nil {
+		return nil, err
+	}
+	return response.Body, nil
+}
+
+func (hb *HttpBundle) Close() error {
+	return nil
+}
