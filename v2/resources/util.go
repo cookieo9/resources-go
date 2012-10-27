a82e72bc4e24adaa612bea6b069c50f3dc373a4a
cookieo9 <cookieo9@gmail.com>
2012-10-27 06:47:07 -0700

    v2: updated comment about ExecutablePath() to include bug info.

--- v2/resources/util.go
+++ v2/resources/util.go
@@ -9,8 +9,12 @@ import (
 // ExecutablePath returns a system-native path to the currently running
 // executable.
 //
-// If the value of os.Args[0] has been tampered with, this function may
-// give innacurate results.
+// BUG: This code uses a simple and portable technique to determine the
+// path to the currently running executable. If os.Args[0] is tampered
+// with either inside or outside the program, the executable might
+// not be found. When issue 4057 in the Go Standard Library is resolved
+// this function will be unnecessary, and the bug should no longer
+// be present.
 func ExecutablePath() (string, error) {
 	exepath, err := exec.LookPath(os.Args[0])
 	if err != nil {
