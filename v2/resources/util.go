4f43024127105635a397f95be96a70ead099e9cb
cookieo9 <cookieo9@gmail.com>
2013-02-27 19:25:27 -0800

    resources/v2: changed Executable path to use better code in external package.

--- v2/resources/util.go
+++ v2/resources/util.go
@@ -1,26 +1,19 @@
 package resources
 
 import (
-	"os"
-	"os/exec"
+	"bitbucket.org/kardianos/osext"
 	"path/filepath"
 )
 
 // ExecutablePath returns a system-native path to the currently running
 // executable.
 //
-// BUG: This code uses a simple and portable technique to determine the
-// path to the currently running executable. If os.Args[0] is tampered
-// with either inside or outside the program, the executable might
-// not be found. When issue 4057 in the Go Standard Library is resolved
-// this function will be unnecessary, and the bug should no longer
-// be present.
+// NOTE: this function was intended to dissapear when it's functionality
+// would be handled by the stdlib (go issue 4057). Since that is less
+// likely to happen, I will probably leave this here leaving the API
+// alone. It is supported via the "bitbucket.org/kardianos/osext" package.
 func ExecutablePath() (string, error) {
-	exepath, err := exec.LookPath(os.Args[0])
-	if err != nil {
-		return "", err
-	}
-	return filepath.Abs(filepath.Clean(exepath))
+	return osext.Executable()
 }
 
 // IsNotFound returns true if the error given is an error representing
