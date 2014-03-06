d3cf07954c0473346a04048c82cf16c7233791d3
cookieo9 <cookieo9@gmail.com>
2014-03-06 14:43:11 -0800

    resources-go/v1: added package documentation
    Simple description w/ link to source code.

---
+++ doc.go
@@ -0,0 +1,14 @@
+/*
+Package resources provides resource loading for applications.
+
+Data can be loaded from a user-provided search path whose elements may be:
+ - Absolute filesystem locations
+ - Working directory relative locations
+ - Application directory relative locations
+ - Source code directory relative locations
+ - GOPATH relative locations
+ - Zip file contents (including zip-files embedded into the exe)
+
+Source code can be found at https://github.com/cookieo9/resources-go/tree/v1
+*/
+package resources
