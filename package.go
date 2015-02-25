372b9d543b7f845d4e386be0caad559665aba593
Carlos Castillo <cookieo9@gmail.com>
2015-02-24 23:38:02 -0800

    Update package.go

--- package.go
+++ package.go
@@ -37,9 +37,8 @@ func OpenCurrentPackage() (Bundle, error) {
 	_, sfile, _, _ := runtime.Caller(1)
 	if p, err := build.ImportDir(filepath.Dir(sfile), build.FindOnly); err == nil {
 			return &packageBundle{OpenFS(p.Dir).(*fsBundle)}, nil
-	} else {
-		return nil, err
 	}
+	return nil, err
 }
 
 // OpenPackagePath returns a Bundle which accesses files
